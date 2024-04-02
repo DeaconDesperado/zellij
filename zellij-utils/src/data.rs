251abcc7c2023a324d531d0a865f315eda5317fd
Mark Grey <mgthesecond@spotify.com>
2024-04-02 12:03:08 -0400

    corrections to exit code coloration

--- zellij-utils/src/data.rs
+++ zellij-utils/src/data.rs
@@ -1145,7 +1145,7 @@ impl From<Palette> for Styling {
                 palette.cyan,
                 palette.green,
                 palette.magenta,
-                palette.bg,
+                palette.black,
             ],
             text_selected: [
                 palette.white,
@@ -1184,6 +1184,20 @@ impl From<Palette> for Styling {
                 palette.gold,
                 palette.silver,
                 palette.purple,
+            ],
+            frame_unselected: [
+                palette.orange,
+                palette.pink,
+                palette.gray,
+                palette.brown,
+                palette.black,
+            ],
+            frame_selected: [
+                palette.green,
+                palette.orange,
+                palette.cyan,
+                palette.magenta,
+                palette.brown,
             ],
             ..Default::default() /*
                                  table_title: (),
